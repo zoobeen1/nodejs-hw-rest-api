eb97a97fa3a4c003c5af6e378727f34ee2b24f24
Vlad Zubin <zoobeen1@gmail.com>
2023-12-13 15:29:34 +0200

    fix app.js

--- app.js
+++ app.js
@@ -1,18 +1,13 @@
-const express = require('express')
-const logger = require('morgan')
-const cors = require('cors')
+const express = require('express');
+const logger = require('morgan');
+const cors = require('cors');
 
-<<<<<<< HEAD
 const contactsRouter = require('./routes');
-=======
-const contactsRouter = require('./routes/api/contacts')
->>>>>>> parent of ba59c45 (The main functionality is ready)
 
-const app = express()
+const app = express();
 
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
+const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 
-<<<<<<< HEAD
 // Middlewares
 app.use(logger(formatsLogger));
 app.use(cors());
@@ -24,20 +19,8 @@ app.use((req, res) => {
   res.status(404).json({ message: 'Path Not found' });
 });
 // Global Errors
-=======
-app.use(logger(formatsLogger))
-app.use(cors())
-app.use(express.json())
-
-app.use('/api/contacts', contactsRouter)
-
-app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' })
-})
-
->>>>>>> parent of ba59c45 (The main functionality is ready)
 app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message })
-})
+  res.status(500).json({ message: err.message });
+});
 
-module.exports = app
+module.exports = app;
