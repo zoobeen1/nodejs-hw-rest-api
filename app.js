8b0e3a844479450e8c57b4c1a395b7944c43cc4a
Vlad Zubin <zoobeen1@gmail.com>
2023-12-13 15:04:19 +0200

    All works - fix broken repo
    Repair from disk copy

--- app.js
+++ app.js
@@ -2,12 +2,29 @@ const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
 
+<<<<<<< HEAD
+const contactsRouter = require('./routes');
+=======
 const contactsRouter = require('./routes/api/contacts')
+>>>>>>> parent of ba59c45 (The main functionality is ready)
 
 const app = express()
 
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 
+<<<<<<< HEAD
+// Middlewares
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
+// Router
+app.use('/api/contacts', contactsRouter);
+// 404
+app.use((req, res) => {
+  res.status(404).json({ message: 'Path Not found' });
+});
+// Global Errors
+=======
 app.use(logger(formatsLogger))
 app.use(cors())
 app.use(express.json())
@@ -18,6 +35,7 @@ app.use((req, res) => {
   res.status(404).json({ message: 'Not found' })
 })
 
+>>>>>>> parent of ba59c45 (The main functionality is ready)
 app.use((err, req, res, next) => {
   res.status(500).json({ message: err.message })
 })
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,31 @@
-const express = require('express')
+const express = require("express");
+const models = require("../../models/contacts.js");
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", async (req, res, next) => {
+  const contacts = await models.listContacts();
+  res.json(contacts);
+});
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", async (req, res, next) => {
+  const ID = req.params.contactId;
+  // console.log(ID);
+  const contact = await models.getContactById(ID);
+  if (contact) res.json(contact);
+  else res.status(404).json({ message: "Not found" });
+});
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.delete("/:contactId", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.put("/:contactId", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
 
-module.exports = router
+module.exports = router;
