9493277b42b04f33039b1f4e266322b6c5123da4
Vlad Zubin <zoobeen1@gmail.com>
2023-12-14 11:05:10 +0200

    Start hw-03

--- utils/contactsValidators.js
+++ utils/contactsValidators.js
@@ -3,7 +3,7 @@ const Joi = require('joi');
 exports.createContactValidator = (data) =>
   Joi.object()
     .keys({
-      name: Joi.string().min(3).max(15).required(),
+      name: Joi.string().min(3).max(30).required(),
       email: Joi.string()
         .email({
           minDomainSegments: 2,
