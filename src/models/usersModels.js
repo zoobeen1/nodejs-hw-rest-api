bcb8f5ed5d04dabe6c46d44445a5845b79d3d727
Vlad Zubin <zoobeen1@gmail.com>
2023-12-20 14:01:21 +0200

    Stated hw-04. Added userModel

---
+++ src/models/usersModels.js
@@ -0,0 +1,32 @@
+const { model, Schema } = require('mongoose');
+
+const userSchema = new Schema(
+  {
+    password: {
+      type: String,
+      required: [true, 'Password is required'],
+    },
+    email: {
+      type: String,
+      required: [true, 'Email is required'],
+      unique: true,
+    },
+    subscription: {
+      type: String,
+      enum: ['starter', 'pro', 'business'],
+      default: 'starter',
+    },
+    token: {
+      type: String,
+      default: null,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+
+const User = model('User', userSchema);
+
+module.exports = User;
